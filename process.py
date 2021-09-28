3c37c804c916602e4717ea73c84f8fdd010fc37a
kazuki <kaz.tokyo.tech20@gmail.com>
2021-09-28 16:40:02 +0900

    add new data

---
+++ process.py
@@ -0,0 +1,23 @@
+import json
+import urllib.request
+
+
+def main():
+    PATH: str = 'scraping/course.json'
+    with open(PATH, 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+        for index in range(len(json_data)):
+            url = 'https://titechinfo-data.s3-ap-northeast-1.amazonaws.com/course-review-tmp/course/' + json_data[index]['id'] + '.json'
+            try:
+                with urllib.request.urlopen(url):
+                    json_data[index]['isExist'] = True
+                    print(True)
+            except urllib.error.URLError as err:
+                json_data[index]['isExist'] = False
+                print(False)
+        with open('course_data.json', 'w', encoding="utf-8") as f:
+            json.dump(json_data, f, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    main()
